5743e8e4201179131ca8289dc15a5416d99218f6
prathyush-sudo <prathyush.ert@gmail .com>
2026-04-20 17:51:14 +0530

    finding sqRT OF NUMBER

---
+++ main.py
@@ -0,0 +1,6 @@
+#input of user
+number = int(input("Enter a number to find its square: "))
+#finding the square of the number
+sQrt = number ** 2
+#printing the result
+print("The square of", number, "is", sQrt)
